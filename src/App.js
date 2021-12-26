91bfac55841cdddb540456f671ed1ffbb1155c6a
Jose Lozano <XxdaftnemesisxX@gmail.com>
2021-12-25 23:37:39 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Te amo mi amor, esto va a ser hecho con mucho amor <3
+          {'Te amo mi amor, esto va a ser hecho con mucho amor <3'}
         </a>
       </header>
     </div>
